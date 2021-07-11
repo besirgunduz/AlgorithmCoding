6b100b138a56516337a92f4857a6d7f555f14fec
Besir Gunduz <besirgunduz1993@gmail.com>
2021-07-11 17:19:03 +0300

    added RomanToInteger

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace LongestCommonPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = { "flower", "flow", "flight","flc","fmm" }; //f
+
+            LongestCommonPrefix(str);
+
+            string LongestCommonPrefix(string[] strs)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                int enKucuk = int.MaxValue; //3
+
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    if (strs[i].Length<enKucuk)
+                    {
+                        enKucuk = strs[i].Length;
+                    }
+                }
+
+                for (int i = 0; i < enKucuk; i++)
+                {
+                    for (int j = 0; j < strs.Length; j++)
+                    {
+                        if (strs[j][i] == strs[1][i])
+                        {
+
+                        }
+                    }
+                }
+
+                string sonuc = sb.ToString();
+
+                return sonuc;
+            }
+        }
+    }
+}
---
+++ RomanToInteger/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RomanToInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+            //  Symbol      Value
+            //  I             1
+            //  V             5
+            //  X             10
+            //  L             50
+            //  C             100
+            //  D             500
+            //  M             1000
+
+            //Input: s = "III"
+            //Output: 3
+            //
+            //Input: s = "LVIII"
+            //Output: 58
+
+            string s = "LVIII";
+            int result = 0;
+
+            var dic = new Dictionary<char, int>
+            {
+                {'I',1 },{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000} //VI = 6
+            };
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 < s.Length && dic[s[i]] < dic[s[i + 1]])
+                {
+                    result -= dic[s[i]];
+                }
+                else
+                {
+                    result += dic[s[i]];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
