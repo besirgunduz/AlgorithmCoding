8ef3ad730e1ed9fc0825ae27320326376e4ecce7
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-13 20:01:20 +0300

    refactoring

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TwoSum
 {
@@ -15,29 +16,60 @@ namespace TwoSum
             //Output: Because nums[0] +nums[1] == 9, we return [0, 1]
 
             int[] nums = { 2, 7, 11, 15 };
-            int target = 8;
+            int target = 9;
 
+            //1.yol
+            var result = TwoSum(nums, target);
+            Console.WriteLine($"[{result[0]},{result[1]}]");
+
+            //2.yol
+            var result2 = TwoSum2(nums, target);
+            Console.WriteLine($"[{result2[0]},{result2[1]}]");
+
+            Console.ReadKey();
+        }
+
+        private static int[] TwoSum(int[] nums, int target)
+        {
             int[] arr = new int[2];
 
             for (int i = 0; i < nums.Length; i++)
             {
                 for (int j = i + 1; j < nums.Length; j++)
                 {
-                    if (nums[i]+nums[j]==target)
+                    if (nums[i] + nums[j] == target)
                     {
                         arr[0] = i;
                         arr[1] = j;
+                        break;
                     }
                 }
             }
 
-            Console.WriteLine($"[{arr[0]},{arr[1]}]");
-
-            Console.ReadKey();
-
-
-
-
+            return arr;
         }
+        private static int[] TwoSum2(int[] nums, int target)
+        {
+            int[] arr = new int[2];
+
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int difference = target - nums[i];
+                if (dic.ContainsKey(difference))
+                {
+                    arr[0] = dic[difference];
+                    arr[1] = i;
+                    break;
+                }
+
+                dic[nums[i]] = i;
+            }
+
+            return arr;
+        }
+
+
     }
 }
