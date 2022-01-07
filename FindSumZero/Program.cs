22ce76da15fe7dbd3994ab4f1b535752fffd6b5a
Besir Gunduz <besirgunduz1993@gmail.com>
2022-01-07 22:31:56 +0300

    Added FindSumZero

---
+++ FindSumZero/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FindSumZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+            //Input: n = 5
+            //Output:[-7,-1,1,3,4]
+            //Explanation: These arrays also are accepted[-5, -1, 1, 2, 3] , [-3,-1,2,-2,4].
+
+            //Input: n = 1
+            //Output:[0]
+
+            var n = 25;
+            Console.WriteLine(string.Join(",", SumZero(n)));
+        }
+
+        private static int[] SumZero(int n)
+        {
+            var result = new int[n];
+            var start = 0;
+            var end = n - 1;
+            var count = 1;
+
+            while (start < end)
+            {
+                result[start] = count;
+                result[end] = -count;
+
+                count++;
+                start++;
+                end--;
+            }
+
+            return result;
+        }
+    }
+}
