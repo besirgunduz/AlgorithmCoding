b15c0e98b6ae39b897e40611e8874592ba7257c3
Besir Gunduz <besirgunduz1993@gmail.com>
2022-01-18 23:53:36 +0300

    Added CanPlaceFlowers

---
+++ CanPlaceFlowers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CanPlaceFlowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+            //Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, 
+            //return if n new flowers can be planted in the flowerbed without violating the no-adjacent - flowers rule.
+
+            //Input: flowerbed = [1, 0, 0, 0, 1], n = 1
+            //Output: true
+        }
+    }
+}
