b62e8688af3d23468a586e5b9ba51358528400ee
Besir Gunduz <besirgunduz1993@gmail.com>
2022-01-20 23:22:28 +0300

    Added ThreeConsecutiveOdds

---
+++ ThreeConsecutiveOdds/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreeConsecutiveOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
