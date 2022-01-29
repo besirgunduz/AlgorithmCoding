c5d3910370f82470a78e9db2d3eb58f6d04dccfd
Besir Gunduz <besirgunduz1993@gmail.com>
2022-01-29 20:05:39 +0300

    Update FactorialTrailingZeroes

--- FactorialTrailingZeroes/Program.cs
+++ FactorialTrailingZeroes/Program.cs
@@ -6,7 +6,26 @@ namespace FactorialTrailingZeroes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Given an integer n, return the number of trailing zeroes in n!.
+
+            //Input: n = 5
+            //Output: 1
+            //Explanation: 5! = 120, one trailing zero.
+
+            Console.WriteLine(TrailingZeroes(5));
+        }
+
+        private static int TrailingZeroes(int n)
+        {
+            var counter = 0;
+
+            while (n > 0)
+            {
+                n /= 5;
+                counter += n;
+            }
+
+            return counter;
         }
     }
 }
