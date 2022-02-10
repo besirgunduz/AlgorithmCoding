431a1b2a7a92a8f0ec97dce1d31f99336f27ba04
Besir Gunduz <besirgunduz1993@gmail.com>
2022-02-10 20:13:21 +0300

    Update ExcelSheetColumnTitle

--- ExcelSheetColumnTitle/Program.cs
+++ ExcelSheetColumnTitle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace ExcelSheetColumnTitle
 {
@@ -6,7 +7,31 @@ namespace ExcelSheetColumnTitle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+            //Input: columnNumber = 28
+            //Output: "AB"
+
+            //Input: columnNumber = 701
+            //Output: "ZY"
+
+            Console.WriteLine(ConvertToTitle(701));
+        }
+
+        private static string ConvertToTitle(int columnNumber)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var sb = new StringBuilder();
+
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                var addedChar = columnNumber % 26;
+                sb.Insert(0, chars[addedChar]);
+                columnNumber /= 26;
+            }
+
+            return sb.ToString();
         }
     }
 }
