b5045c0e8cee66ff02a0d34843ebc3cba8724cba
Besir Gunduz <besirgunduz1993@gmail.com>
2022-02-12 12:26:56 +0300

    Update SquareIsWhite

--- SquareIsWhite/Program.cs
+++ SquareIsWhite/Program.cs
@@ -6,7 +6,21 @@ namespace SquareIsWhite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Determine Color of a Chessboard Square
+            //Return true if the square is white, and false if the square is black. 
+            //The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
+
+            //Input: coordinates = "a1"
+            //Output: false
+
+            Console.WriteLine(SquareIsWhite("h3"));
+        }
+        private static bool SquareIsWhite(string coordinates)
+        {
+            var harf = coordinates[0] - 'a';
+            var rakam = coordinates[1] - '0';
+
+            return (harf + rakam) % 2 == 0;
         }
     }
 }
