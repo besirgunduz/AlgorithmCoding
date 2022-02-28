d93b630c6a1997be67e9d383e77380eef5f933ec
Besir Gunduz <besirgunduz1993@gmail.com>
2022-02-28 20:57:07 +0300

    Update JewelsAndStones

--- JewelsAndStones/Program.cs
+++ JewelsAndStones/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JewelsAndStones
 {
@@ -6,7 +7,30 @@ namespace JewelsAndStones
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have.
+            //Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+            //Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+            //Input: jewels = "aA", stones = "aAAbbbb"
+            //Output: 3
+
+            Console.WriteLine(NumJewelsInStones("aA", "aAAbbbb"));
+        }
+
+        private static int NumJewelsInStones(string jewels, string stones)
+        {
+            var count = 0;
+            var hashSat = new HashSet<char>(jewels);
+
+            foreach (var s in stones)
+            {
+                if (hashSat.Contains(s))
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
     }
 }
