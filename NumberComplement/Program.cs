cf4d4775821580ea561d7227783d0247305ceb1f
Besir Gunduz <besirgunduz1993@gmail.com>
2022-03-01 23:50:05 +0300

    Added NumberComplement

---
+++ NumberComplement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberComplement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+            //For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+
+        }
+    }
+}
