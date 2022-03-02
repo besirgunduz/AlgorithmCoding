cff280349a30e34b71e024ffaa9c1e482d54fcf8
Besir Gunduz <besirgunduz1993@gmail.com>
2022-03-03 00:10:49 +0300

    Update FirstUniqueChar

--- FirstUniqueChar/Program.cs
+++ FirstUniqueChar/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FirstUniqueChar
 {
@@ -6,7 +7,39 @@ namespace FirstUniqueChar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+            //Input: s = "loveleetcode"
+            //Output: 2
+
+            Console.WriteLine(FirstUniqueChar("loveleetcode"));
+        }
+
+        private static int FirstUniqueChar(string s)
+        {
+            var dic = new Dictionary<char, int>();
+
+            foreach (char item in s)
+            {
+                if (dic.ContainsKey(item))
+                {
+                    dic[item]++;
+                }
+                else
+                {
+                    dic.Add(item, 1);
+                }
+            }
+
+            foreach (var item in s)
+            {
+                if (dic[item] == 1)
+                {
+                    return s.IndexOf(item);
+                }
+            }
+
+            return -1;
         }
     }
 }
