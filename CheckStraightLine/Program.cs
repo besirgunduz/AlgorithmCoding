a64f993bf96a8e0213ce78e95f45dc642415ec13
Besir Gunduz <besirgunduz1993@gmail.com>
2022-03-15 23:11:16 +0300

    Added CheckStraightLine

--- CheckStraightLine/Program.cs
+++ CheckStraightLine/Program.cs
@@ -6,7 +6,29 @@ namespace CheckStraightLine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point.
+            //Check if these points make a straight line in the XY plane.
+
+            //Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+            //Output: true
+
+        }
+        public static bool CheckStraightLine(int[][] c)
+        {
+
+            if (c.Length < 2) return true;
+
+            for (int i = 0; i < c.Length - 2; i++)
+            {
+                if ((c[i + 2][1] - c[i + 1][1]) * (c[i + 1][0] - c[i][0]) !=
+                  (c[i + 1][1] - c[i][1]) * (c[i + 2][0] - c[i + 1][0]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
         }
     }
 }
