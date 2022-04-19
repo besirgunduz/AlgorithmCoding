cd990653cbae548d90b6804b7d55fefdfa8f4d24
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-04-19 21:57:19 +0300

    Added FindTownJudge

---
+++ FindTownJudge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindTownJudge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
