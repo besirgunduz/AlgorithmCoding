553b4a741eafd105d668ffe983a4fa5255bb37ce
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-04-24 04:42:15 +0300

    Add SingleElement

---
+++ AlgorithmCoding/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AlgorithmCoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = SingleElement.SingleNonDublicate(new int[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 });
+            Console.WriteLine(result);
+        }
+    }
+}
