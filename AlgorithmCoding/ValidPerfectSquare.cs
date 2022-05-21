20fca11c7cf3820d33e31fc35590dc91e11d9ff1
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-05-21 14:17:53 +0300

    Add ValidPerfectSquare

---
+++ AlgorithmCoding/ValidPerfectSquare.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCoding
+{
+    public static class ValidPerfectSquare
+    {
+        //Given a positive integer num, write a function which returns True if num is a perfect square else False.
+        //Follow up: Do not use any built -in library function such as sqrt.
+
+        //Input: num = 16
+        //Output: true
+
+        public static bool IsPerfectSquare(int num)
+        {
+            long left = 1;
+            long right = num;
+            while (left <= right)
+            {
+                long middle = (right - left) / 2 + left;
+
+                if (middle * middle == num) return true;
+                if (middle * middle > num) right = middle - 1;
+                else left = middle + 1;
+            }
+
+            return false;
+        }
+
+    }
+}
