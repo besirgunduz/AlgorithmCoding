80ffe76bf89e601b23d4175582819fe7efee36ad
BESIR GUNDUZ <BESIR.GUNDUZ@lcwaikiki.com>
2022-05-22 14:09:07 +0300

    Add ExcelSheetColumnTitle

---
+++ AlgorithmCoding/ExcelSheetColumnTitle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCoding
+{
+    public static class ExcelSheetColumnTitle
+    {
+        //Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+
+        //Input: columnNumber = 28
+        //Output: "AB"
+
+        //Input: columnNumber = 701
+        //Output: "ZY"
+
+        public static string ConvertToTitle(int columnNumber)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var sb = new StringBuilder();
+
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                var addedChar = columnNumber % 26;
+                sb.Insert(0, chars[addedChar]);
+                columnNumber /= 26;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
