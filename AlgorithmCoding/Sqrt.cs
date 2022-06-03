68fbf8db270449eb4074fa6fea20e1f5eaa99884
Besir Gunduz <besirgunduz1993@gmail.com>
2022-06-03 22:19:27 +0300

    Update sqrt

---
+++ AlgorithmCoding/Sqrt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCoding
+{
+    public static class Sqrt
+    {
+        //Given a non-negative integer x, compute and return the square root of x.
+        //Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+        //Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5)
+
+        //Input: x = 8
+        //Output: 2
+
+        public static int SqrtResult(int x)
+        {
+            int left = 1;
+            int right = x;
+
+            while (right >= left)
+            {
+                int middle = (right - left) / 2 + left;
+
+                if (x / middle == middle)
+                    return middle;
+
+                else if (x / middle < middle)
+                    right = middle - 1;
+
+                else
+                    left = middle + 1;
+            }
+
+            return right;
+        }
+    }
+}
