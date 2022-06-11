579c3efe70f79c526637d58c5e26c7676d0e9e78
Besir Gunduz <besirgunduz1993@gmail.com>
2022-06-11 14:34:45 +0300

    update TribonacciNumber

---
+++ AlgorithmCoding/TribonacciNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCoding
+{
+    public static class TribonacciNumber
+    {
+        //The Tribonacci sequence Tn is defined as follows: 
+        //T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn + 1 + Tn + 2 for n >= 0.
+        //Given n, return the value of Tn.
+
+        //Input: n = 4
+        //Output: 4
+
+        //Input: n = 25
+        //Output: 1389537
+
+        public static int TribonacciResult(int n)
+        {
+            var arr = new int[n + 3];
+            arr[0] = 0;
+            arr[1] = 1;
+            arr[2] = 1;
+
+            for (int i = 3; i < arr.Length; i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+            }
+
+            return arr[n];
+        }
+    }
+}
