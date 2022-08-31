8b9d9369e5724afac86a256aa91cf6afcb9709b0
Besir Gunduz <besirgunduz1993@gmail.com>
2022-08-31 22:40:55 +0300

    RansomNote

--- AlgorithmCoding/Program.cs
+++ AlgorithmCoding/Program.cs
@@ -7,7 +7,8 @@ namespace AlgorithmCoding
         static void Main(string[] args)
         {
             //Console.WriteLine(FirstBadVersion.FirstBadVersionResult(10));
-            Console.WriteLine(JewelsAndStones.JewelsAndStonesResult("aA", "aAAbbbb"));
+            //Console.WriteLine(JewelsAndStones.JewelsAndStonesResult("aA", "aAAbbbb"));
+            Console.WriteLine(RansomNote.RansomNoteResult("aa","aab"));
         }
     }
 }
---
+++ AlgorithmCoding/RansomNote.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorithmCoding
+{
+    public static class RansomNote
+    {
+        //Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+        //Each letter in magazine can only be used once in ransomNote.
+
+        //Input: ransomNote = "aa", magazine = "aab"
+        //Output: true
+
+        //Input: ransomNote = "aa", magazine = "ab"
+        //Output: false
+
+        public static bool RansomNoteResult(string ransomNote, string magazine)
+        {
+            var dic = new Dictionary<char, int>();
+
+            foreach (var item in magazine)
+            {
+                if (dic.ContainsKey(item))
+                {
+                    dic[item]++;
+                }
+                else
+                {
+                    dic.Add(item, 1);
+                }
+            }
+
+            foreach (var item in ransomNote)
+            {
+                if (!dic.ContainsKey(item) || dic[item] <= 0)
+                {
+                    return false;
+                }
+
+                dic[item]--;
+            }
+
+            return true;
+        }
+    }
+}
