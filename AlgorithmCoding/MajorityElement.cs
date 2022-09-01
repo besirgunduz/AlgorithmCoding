ecf62f991aa50b9c91a24db11a62a0254d64c7ae
Besir Gunduz <besirgunduz1993@gmail.com>
2022-09-01 21:17:22 +0300

    MajorityElement

--- AlgorithmCoding/MajorityElement.cs
+++ AlgorithmCoding/MajorityElement.cs
@@ -1,21 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace MajorityElement
+namespace AlgorithmCoding
 {
-    internal class Program
+    public static class MajorityElement
     {
-        static void Main(string[] args)
-        {
-            //Given an array nums of size n, return the majority element.
-            //The majority element is the element that appears more than ⌊n / 2⌋ times.You may assume that the majority element always exists in the array.
+        //Given an array nums of size n, return the majority element.
+        //The majority element is the element that appears more than ⌊n / 2⌋ times.You may assume that the majority element always exists in the array.
 
-            //Input: nums = [2,2,1,1,1,2,2]
-            //Output: 2
+        //Input: nums = [2,2,1,1,1,2,2]
+        //Output: 2
 
-            Console.WriteLine(MajorityElement(new int[] { 2, 2, 1, 1, 1, 2, 2 }));
-        }
-        public static int MajorityElement(int[] nums)
+        public static int MajorityElementResult(int[] nums)
         {
             if (nums.Length == 1)
             {
@@ -36,9 +35,11 @@ namespace MajorityElement
                 }
             }
 
+            var halfSize = nums.Length / 2;
+
             foreach (var key in dic.Keys)
             {
-                if (dic[key] > nums.Length / 2)
+                if (dic[key] > halfSize)
                 {
                     return key;
                 }
--- AlgorithmCoding/Program.cs
+++ AlgorithmCoding/Program.cs
@@ -10,7 +10,8 @@ namespace AlgorithmCoding
             //Console.WriteLine(JewelsAndStones.JewelsAndStonesResult("aA", "aAAbbbb"));
             //Console.WriteLine(RansomNote.RansomNoteResult("aa","aab"));
             //Console.WriteLine(NumberComplement.NumberComplementResult(5));
-            Console.WriteLine(FirstUniqueCharacter.FirstUniqueCharacterResult("loveleetcode"));
+            //Console.WriteLine(FirstUniqueCharacter.FirstUniqueCharacterResult("loveleetcode"));
+            Console.WriteLine(MajorityElement.MajorityElementResult(new int[] {2,2,3,3,4,2,2,2}));
         }
     }
 }
