493cb6ae23d75d3724e39f5602f8ec8b95a79197
Besir Gunduz <besirgunduz1993@gmail.com>
2023-01-23 23:09:02 +0300

    Added SortArrayByParity

---
+++ AlgorithmCoding/SortArrayByParity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCoding
+{
+    public class SortArrayByParity
+    {
+        //Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+        //Return any array that satisfies this condition.
+
+        //Input: nums = [3,1,2,4]
+        //Output: [2,4,3,1]
+
+        public static int[] SortArrayByParityResult(int[] nums)
+        {
+            var index = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    var temp = nums[index];
+                    nums[index] = nums[i];
+                    nums[i] = temp;
+                    index++;
+                }
+            }
+
+            return nums;
+        }
+    }
+}
