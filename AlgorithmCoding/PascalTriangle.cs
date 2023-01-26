5a6de2035da194cb7e00b48bfbab71d6058cfc0d
Besir Gunduz <besirgunduz1993@gmail.com>
2023-01-26 23:59:21 +0300

    Update PascalTriangle

--- AlgorithmCoding/PascalTriangle.cs
+++ AlgorithmCoding/PascalTriangle.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AlgorithmCoding
+﻿namespace AlgorithmCoding
 {
     public class PascalTriangle
     {
+
     }
 }
