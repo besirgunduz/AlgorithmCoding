0a0cf861df9358d92e6aa96aaf51fb657c0b4f8a
Besir Gunduz <besirgunduz1993@gmail.com>
2023-02-09 00:14:58 +0300

    Update ReverseLinkedList

--- AlgorithmCoding/ReverseLinkedList.cs
+++ AlgorithmCoding/ReverseLinkedList.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AlgorithmCoding
+﻿namespace AlgorithmCoding
 {
     public class ReverseLinkedList
     {
+        //Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+        //Input: head = [1,2,3,4,5]
+        //Output: [5,4,3,2,1]
+
+        public static ListNode ReverseList(ListNode head)
+        {
+            ListNode previous = null;
+
+            while (head != null)
+            {
+                var next = head.next;
+                head.next = previous;
+                previous = head;
+                head = next;
+            }
+
+            return previous;
+        }
     }
 }
