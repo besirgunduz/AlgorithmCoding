126971c56553542d4facbdd7c34d83d51f5db2c7
Besir Gunduz <besirgunduz1993@gmail.com>
2023-02-14 23:19:56 +0300

    Update GlobalAndLocalInversions

--- AlgorithmCoding/GlobalAndLocalInversions.cs
+++ AlgorithmCoding/GlobalAndLocalInversions.cs
@@ -8,5 +8,34 @@ namespace AlgorithmCoding
 {
     public class GlobalAndLocalInversions
     {
+        //You are given an integer array nums of length n which represents a permutation of all the integers in the range [0, n - 1].
+
+        //The number of global inversions is the number of the different pairs(i, j) where:
+        //0 <= i<j<n
+        //nums[i]> nums[j]
+
+        //The number of local inversions is the number of indices i where:
+        //0 <= i<n - 1
+        //nums[i]> nums[i + 1]
+
+        //Return true if the number of global inversions is equal to the number of local inversions.
+
+        //Input: nums = [1,2,0]
+        //Output: false
+
+        public static bool IsIdealPermutation(int[] nums)
+        {
+            var max = -1;
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                max = Math.Max(nums[i], max);
+
+                if (max > nums[i + 2])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
