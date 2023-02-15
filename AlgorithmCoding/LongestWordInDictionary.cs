9f9917d52defb2e5cf18009af51b1612ecc67a21
Besir Gunduz <besirgunduz1993@gmail.com>
2023-02-15 22:14:03 +0300

    Update LongestWordInDictionary

--- AlgorithmCoding/LongestWordInDictionary.cs
+++ AlgorithmCoding/LongestWordInDictionary.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AlgorithmCoding
 {
     public class LongestWordInDictionary
     {
+        //Given an array of strings words representing an English Dictionary, return the longest word in words that can be built one character at a time by other words in words.
+        //If there is more than one possible answer, return the longest word with the smallest lexicographical order.If there is no answer, return the empty string.
+        //Note that the word should be built from left to right with each additional character being added to the end of a previous word. 
+
+        //Input: words = ["a","banana","app","appl","ap","apply","apple"]
+        //Output: "apple"
+
+        public static string LongestWord(string[] words)
+        {
+            Array.Sort(words);
+            var hashSet = new HashSet<string>();
+            var result = String.Empty;
+
+            foreach (var word in words)
+            {
+                if (word.Length == 1 || hashSet.Contains(word.Substring(0, word.Length - 1)))
+                {
+                    if (word.Length > result.Length)
+                        result = word;
+
+                    hashSet.Add(word);
+                }
+            }
+            return result;
+        }
+
     }
 }
