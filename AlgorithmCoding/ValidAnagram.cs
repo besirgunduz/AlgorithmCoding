48d109777745f6b7f50e854fd317b0ea41a61665
Besir Gunduz <besirgunduz1993@gmail.com>
2023-04-04 23:54:05 +0300

    Update ValidAnagram

--- AlgorithmCoding/ValidAnagram.cs
+++ AlgorithmCoding/ValidAnagram.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AlgorithmCoding
+﻿namespace AlgorithmCoding
 {
-    internal class ValidAnagram
+    public class ValidAnagram
     {
+        //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+        //An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+        //typically using all the original letters exactly once.
+
+        //Input: s = "anagram", t = "nagaram"
+        //Output: true
+
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            var charCount = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                charCount[s[i] - 'a']++;
+                charCount[t[i] - 'a']--;
+            }
+
+            foreach (var count in charCount)
+            {
+                if (count != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
